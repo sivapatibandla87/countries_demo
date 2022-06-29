efd8b583636dd5d151686bc3243b7fd89defc135
sivapatibandla87 <patibandlas.java@gmail.com>
2022-06-29 13:23:05 +0530

    files updated

---
+++ src/main/java/com/country/population/CountryPopulationApplication.java
@@ -0,0 +1,13 @@
+package com.country.population;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryPopulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryPopulationApplication.class, args);
+	}
+
+}
